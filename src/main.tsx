7d9a78a59dfe37100129425b065a9f68804d38bf
tomfreire2025 <ourfaiith@gmail.com>
2025-10-29 22:59:45 -0300

    Change import path to Index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Index from './pages/Index'
+import Index from './pages/Index.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
